d4373bde517e351a06e534cac4c6303eba077dd8
Ameet Jayawant <ameetj85@gmail.com>
2023-01-20 14:56:26 -0500

    Ported over use-embedded-webview from old repo which is named embedding-wpf-demo-master

---
+++ how-to/automation-testing/app/MainWindow.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace OpenFin.Automation.Win.Sample
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.embeddedViewPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.embeddedView = new Openfin.WinForm.EmbeddedView();
+            this.embeddedViewPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // embeddedViewPanel
+            // 
+            this.embeddedViewPanel.Controls.Add(this.label1);
+            this.embeddedViewPanel.Controls.Add(this.embeddedView);
+            this.embeddedViewPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.embeddedViewPanel.Location = new System.Drawing.Point(0, 0);
+            this.embeddedViewPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.embeddedViewPanel.Name = "embeddedViewPanel";
+            this.embeddedViewPanel.Size = new System.Drawing.Size(1206, 1136);
+            this.embeddedViewPanel.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(17, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loading content, please wait...";
+            // 
+            // embeddedView
+            // 
+            this.embeddedView.AutoScale = true;
+            this.embeddedView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.embeddedView.Location = new System.Drawing.Point(0, 0);
+            this.embeddedView.Margin = new System.Windows.Forms.Padding(2);
+            this.embeddedView.Name = "embeddedView";
+            this.embeddedView.OpenfinWindow = null;
+            this.embeddedView.Size = new System.Drawing.Size(1206, 1136);
+            this.embeddedView.TabIndex = 0;
+            this.embeddedView.TabStop = false;
+            this.embeddedView.Text = "embeddedView3";
+            this.embeddedView.Ready += new System.EventHandler(this.embeddedView_Ready);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(31)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(1206, 1136);
+            this.Controls.Add(this.embeddedViewPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "MainWindow";
+            this.Text = "Automation Example Tool";
+            this.embeddedViewPanel.ResumeLayout(false);
+            this.embeddedViewPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel embeddedViewPanel;
+        private Openfin.WinForm.EmbeddedView embeddedView;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ how-to/use-the-embedded-webview/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json.Linq;
+using Openfin.Desktop.Messaging;
+using Fin = Openfin.Desktop;
+
+namespace embedd_wpf_demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string RuntimeVersion = "19.89.57.15";
+        const string AppUuid = "hyper-grid-uuid";
+        const string AppName = "hyper-grid";
+        const string ChannelName = "user-data";
+        const string DataChangeTopic = "data-updated";
+        const string SelectionChangeTopic = "selection-changed";
+
+        List<Person> peopleData;
+
+        Fin.Messaging.ChannelClient channelClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Runtime options is how we set up the OpenFin Runtime environment
+            peopleData = PeopleData.Get();
+
+            var runtimeOptions = new Fin.RuntimeOptions
+            {
+                Version = RuntimeVersion,
+                EnableRemoteDevTools = true,
+                RuntimeConnectTimeout = 20000
+            };
+
+            var fin = Fin.Runtime.GetRuntimeInstance(runtimeOptions);
+
+            fin.Error += (sender, e) =>
+            {
+                Console.Write(e);
+            };
+
+            fin.Connect(() =>
+            {
+                // Initialize the communication channel after the runtime has connected
+                // but before launching any applications or EmbeddedViews
+
+                var channelProvider = fin.InterApplicationBus.Channel.CreateProvider(ChannelName);
+                channelProvider.RegisterTopic<string, bool>(SelectionChangeTopic, OnSelectionChanged);
+                channelProvider.ClientConnected += ChannelProvider_ClientConnected;
+                channelProvider.OpenAsync().Wait();
+            });
+
+            //Initialize the grid view by passing the runtime Options and the ApplicationOptions
+            var fileUri = new Uri(System.IO.Path.GetFullPath(@"..\..\web-content\index.html")).ToString();
+            OpenFinEmbeddedView.Initialize(runtimeOptions, new Fin.ApplicationOptions(AppName, AppUuid, fileUri));
+
+            //Once the grid is ready get the data and populate the list box.
+            OpenFinEmbeddedView.Ready += (sender, e) =>
+            {
+                //set up the data
+                var peopleInStates = (from person in peopleData
+                                      group person by person.BirthState into stateGroup
+                                      select new
+                                      {
+                                          StateName = stateGroup.First().BirthState,
+                                          People = stateGroup
+                                      })
+                                      .OrderBy(p => p.StateName)
+                                      .ToList();
+
+                //Any Interactions with the UI must be done in the right thread.
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    peopleInStates.ForEach(state => StatesBox.Items.Add(state.StateName));
+                }), null);
+            };
+        }
+
+        private void ChannelProvider_ClientConnected(object sender, ChannelConnectedEventArgs e)
+        {
+            channelClient = e.Client;
+
+            // There is currently a bug that requires this task to occur on a different
+            // thread than the connected event. Fixed in later versions.
+            System.Threading.ThreadPool.QueueUserWorkItem(o =>
+            {
+                channelClient?.DispatchAsync(DataChangeTopic, peopleData);
+            });
+        }
+
+        private bool OnSelectionChanged(string selection)
+        {
+            Dispatcher.Invoke(new Action(() => { SelectedValue.Text = selection; }));
+            return true;
+        }
+
+        private void States_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //On State selection we will send the data to the grid.
+            var data = (from person in peopleData
+                        where StatesBox.SelectedItems.Contains(person.BirthState)
+                        select person).ToList();
+
+            channelClient?.DispatchAsync(DataChangeTopic, data);
+        }
+    }
+}
