0a3e0e3692fb01ceef939088ef9a2fc4f9817178
John <john.mandia@gmail.com>
2024-01-08 14:59:37 +0000

    Update paths (#25)

--- how-to.v1/integrate-with-workspace/framework/OpenFin.WPF.TestHarness/Settings.cs
+++ how-to.v1/integrate-with-workspace/framework/OpenFin.WPF.TestHarness/Settings.cs
@@ -6,7 +6,7 @@ namespace OpenFin.WPF.TestHarness
         public static string DefaultUUID = "openfin-dotnet6-wpf-test-harness";
         public static string DefaultWorkspaceAutoConnect = "true";
         public static string DefaultWorkspaceConnectionId = "workspace-connection";
-        public static string DefaultWorkspaceUUID = "customize-workspace";
+        public static string DefaultWorkspaceUUID = "workspace-platform-starter";
         public static string DefaultWorkspaceManifestUrl = "http://localhost:8080/manifest.fin.json";
         public static string DefaultLicenseKey = "openfin-demo-license-key";
     }
--- how-to.v1/integrate-with-workspace/framework/OpenFin.WindowsForm.TestHarness/Settings.cs
+++ how-to.v1/integrate-with-workspace/framework/OpenFin.WindowsForm.TestHarness/Settings.cs
@@ -5,7 +5,7 @@
         public static string DefaultUUID = "openfin-dotnet4-winform-test-harness";
         public static string DefaultWorkspaceAutoConnect = "true";
         public static string DefaultWorkspaceConnectionId = "workspace-connection";
-        public static string DefaultWorkspaceUUID = "customize-workspace";
+        public static string DefaultWorkspaceUUID = "workspace-platform-starter";
         public static string DefaultWorkspaceManifestUrl = "http://localhost:8080/manifest.fin.json";
         public static string DefaultLicenseKey = "openfin-demo-license-key";
     }
